fece8ee880e301d3401eada4683d7792c6b49175
Soumyadip Adak <149286607+adak99@users.noreply.github.com>
2025-03-09 20:53:27 +0530

    update

--- PRODUCT MANAGEMENT SYSTEM USING SPRING BOOT AND REACT/productManagementSystem-Back-end/src/main/java/com/soumya/productManagementSystem/repository/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.soumya.productManagementSystem.repository;
-
-import com.soumya.productManagementSystem.entity.ProductEntity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepo extends MongoRepository<ProductEntity, String> {
-}
